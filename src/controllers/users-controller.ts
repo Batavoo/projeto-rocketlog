8a55be9bafe998e22601e4d8caac9835d06f8072
Gustavo Santos Arruda <gsarruda.office@gmail.com>
2025-09-02 10:27:48 -0300

    validando dados do usuário

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,7 +1,27 @@
 import { Request, Response } from "express";
+import z from "zod";
 
 class UsersController {
   async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(2),
+      email: z.string().email(),
+      password: z
+        .string()
+        .min(8, "Password must be at least 8 characters")
+        .refine((password) => /[A-Z]/.test(password), {
+          message: "Password must contain at least one uppercase letter",
+        })
+        .refine((password) => /[0-9]/.test(password), {
+          message: "Password must contain at least one number",
+        })
+        .refine((password) => /[^A-Za-z0-9]/.test(password), {
+          message: "Password must contain at least one special character",
+        }),
+    });
+
+    const { name, email, password } = bodySchema.parse(request.body);
+
     return response.json({ message: "ok" });
   }
 }
